72bb05c072d3a9d4ef440e5c8f9804cfaecb3943
nirmalkar <hemantnirmalkar17@gmail.com>
2023-01-11 12:51:53 +0530

    error handling

--- src/Server/CreateFile.ts
+++ src/Server/CreateFile.ts
@@ -1,12 +1,19 @@
 const fs = require("fs");
+const { execSync } = require("child_process");
 //create empty file of certain size to fill up the disk space: slower way
 export class CreateFile {
   public createEmptyFileOfSize(fileName: string, size: number) {
     return new Promise((resolve, reject) => {
-      const fh = fs.openSync(fileName, "w");
-      fs.writeSync(fh, "ok", Math.max(0, size - 2));
-      fs.closeSync(fh);
-      resolve(true);
+      try {
+        // execSync('fallocate -l 1G file')
+        const fh = fs.openSync(fileName, "w");
+        fs.writeSync(fh, "ok", Math.max(0, size - 2));
+        fs.closeSync(fh);
+        resolve(true);
+      } catch (e) {
+        console.log(e, "Error Occurred writing new empty file");
+        reject(e);
+      }
     });
   }
 }
