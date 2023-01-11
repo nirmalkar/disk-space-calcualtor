0fc7467e6aee0aed42995160346ccc60bff3f7d8
nirmalkar <hemantnirmalkar17@gmail.com>
2023-01-11 15:52:23 +0530

    pretty slow solution

--- src/Server/Server.ts
+++ src/Server/Server.ts
@@ -4,7 +4,7 @@ import { CalculateDiskSpace } from "./spaceCalc";
 export class Server {
   public startServer() {
     const hostname = "127.0.0.1";
-    const port = 3000;
+    const port = 3001;
 
     const server = createServer(async (req: any, res: any) => {
       res.statusCode = 200;
@@ -13,9 +13,9 @@ export class Server {
       
     });
      // Create a file of 1 GiB
-     new CreateFile().createEmptyFileOfSize("./1.txt", 1 * 1024 * 1024 * 1024);
-   const diskPercentFilled = new CalculateDiskSpace().readFile()
-   console.log(diskPercentFilled, 'disk filled!')
+     const diskPercentFilled = new CalculateDiskSpace().readFile()
+     console.log(diskPercentFilled.avail, 'disk filled!')
+    //  new CreateFile().createEmptyFileOfSize("./1.txt", diskPercentFilled.avail * 1024 * 1024 * 1024);
     server.listen(port, hostname, () => {
       console.log(
         `El servidor se está ejecutando en http://${hostname}:${port}/`
--- src/Server/spaceCalc.ts
+++ src/Server/spaceCalc.ts
@@ -1,15 +1,26 @@
 const { execSync } = require("child_process");
-const { readFileSync, unlinkSync } = require("fs");
+const { readFileSync } = require("fs");
 
-execSync("df -h > df.txt");
 export class CalculateDiskSpace {
   public readFile() {
+    execSync("df -h > df.txt");
     const space = readFileSync("df.txt", (data: any, err: any) => {
       if (err) {
         throw err;
       }
-    }).toString().split(' ')
-      const spaceLeftPercentage = space[space.length - 2]
-      return  spaceLeftPercentage
+    })
+      .toString()
+      .split("\n");
+    const values = space[1]
+      .split(" ")
+      .filter((ele: string, i: number) => !isNaN(Number(ele[0])));
+    const array: any = ["size", "used", "avail", "usedPercent"];
+    const obj: any = {};
+    values.forEach((ele: string, index: number) => {
+      if (ele && typeof Number(ele[0]) === "number") {
+        obj[array[index]] = Number(ele.slice(0, ele.length - 1))
+      }
+    });
+    return obj;
   }
 }
