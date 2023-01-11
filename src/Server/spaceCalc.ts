0818e98e66d0656d693cb32314718399bffbfb45
nirmalkar <hemantnirmalkar17@gmail.com>
2023-01-11 17:08:14 +0530

    unit conversion

--- src/Server/spaceCalc.ts
+++ src/Server/spaceCalc.ts
@@ -2,25 +2,40 @@ const { execSync } = require("child_process");
 const { readFileSync } = require("fs");
 
 export class CalculateDiskSpace {
-  public readFile() {
-    execSync("df -h > df.txt");
-    const space = readFileSync("df.txt", (data: any, err: any) => {
-      if (err) {
-        throw err;
-      }
-    })
-      .toString()
-      .split("\n");
-    const values = space[1]
-      .split(" ")
-      .filter((ele: string, i: number) => !isNaN(Number(ele[0])));
-    const array: any = ["size", "used", "avail", "usedPercent"];
-    const obj: any = {};
-    values.forEach((ele: string, index: number) => {
-      if (ele && typeof Number(ele[0]) === "number") {
-        obj[array[index]] = Number(ele.slice(0, ele.length - 1))
-      }
-    });
-    return obj;
+    public convertToBytes(val: string){
+        const sizConst = 1024
+        switch(val.slice(val.length - 1)){
+            case 'T':
+                return Number(val.slice(0, val.length - 1)) *sizConst**4;
+            case 'G':
+                return Number(val.slice(0, val.length - 1)) * sizConst**3;
+            case 'M':
+                return Number(val.slice(0, val.length - 1)) * sizConst**2;
+            case 'K':
+                return Number(val.slice(0, val.length - 1)) * sizConst
+            default:
+                return val
+        }
+    }
+    public readFile() {
+      execSync('df -h > df.txt');
+      const space = readFileSync('df.txt', (_data: any, err: any) => {
+        if (err) {
+          throw err;
+        }
+      })
+        .toString()
+        .split('\n');
+      const values = space[1].split(' ').filter((ele: string) => !isNaN(Number(ele[0])));
+      const array: any = ['size', 'used', 'avail', 'usedPercent'];
+      const obj: any = {};
+      values.forEach((ele: string, index: number) => {
+        
+        if (ele && typeof Number(ele[0]) === 'number') {
+          obj[array[index]] = this.convertToBytes(ele);
+        }
+      });
+      console.log(obj)
+      return obj;
+    }
   }
-}
